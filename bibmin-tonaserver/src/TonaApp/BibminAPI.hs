73f2cc4acf25a6b221afb6d1bbf55d53c879c9ac
yokoyama-haruki <yokoyama.haruki@jp.fujitsu.com>
2019-04-19 22:50:42 +0900

    add tona app

---
+++ bibmin-tonaserver/src/TonaApp/BibminAPI.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module TonaApp.BibminAPI where
+
+import Tonalude
+import Servant.API
+import Data.Aeson
+import GHC.Generics ()
+
+-- | /bibmin?bibtex='@misc{ ... }'
+type BibminAPI = "bibmin" :> Get '[JSON] Mattermost 
+    :<|> "bibmin" :> ReqBody '[JSON] Text :> Post '[JSON] Mattermost
+
+-- | Response data
+data Mattermost = Mattermost
+    { text :: Text
+    } deriving (Generic, ToJSON)
+
+-- | Boilerplate of servant
+api :: Proxy BibminAPI
+api = Proxy
---
+++ bibmin-tonaserver/src/TonaApp/Main.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module TonaApp.Main where
+
+import Tonalude
+import Tonatona (HasConfig(..), HasParser(..))
+import qualified Tonatona.Logger as TonaLogger
+import qualified Tonatona.Servant as TonaServer
+import Servant
+
+import TonaApp.BibminAPI
+
+-- App
+
+
+app :: RIO Config ()
+app = do
+  TonaLogger.logInfo $ display ("This is a bibmin-tonaserver" :: Text)
+  TonaLogger.logDebug $ display ("Start server" :: Text)
+  TonaServer.run @BibminAPI server
+
+server :: ServerT BibminAPI (RIO Config)
+server = getBibmin :<|> postBibmin
+  where
+    getBibmin = return $ Mattermost "usage"
+    postBibmin bib = return $ Mattermost bib
+
+
+-- Config
+
+
+data Config = Config
+  { tonaLogger :: TonaLogger.Config
+  , tonaServer :: TonaServer.Config
+  -- , yetAnotherPlugin :: TonaYetAnotherPlugin.Config
+  }
+
+
+instance HasConfig Config TonaLogger.Config where
+  config = tonaLogger
+
+instance HasConfig Config TonaServer.Config where
+  config = tonaServer
+  
+instance HasParser Config where
+  parser = Config
+      <$> parser
+      <*> parser
+      -- <*> parser
